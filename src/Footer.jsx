e7522ebe57f870f7a90cfe0fa7e742aa60e594df
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2026-02-19 16:32:07 -0300

    Add: Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import './footer.css'
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p className="legal">Todos los derechos reservados. © 2026 - Hecho por Javier Oyarzo. <a href="https://github.com/javiboin">@javiboin</a></p>
+    </footer>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PositionList } from './components/PositionList.jsx'
+import { Footer } from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PositionList />
+    <Footer />
   </StrictMode>,
 )
