a16d956581e72e2424f37596e63b1ee93372abdb
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2026-02-19 00:52:53 -0300

    Add: Listado de Posiciones Disponibles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { PositionList } from "./components/PositionList"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const URL_BASE = 'https://botfilter-h5ddh6dye8exb7ha.centralus-01.azurewebsites.net'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    /* fetch(URL_BASE + '/api/candidate/get-by-email?email=javi_14_228@hotmail.com')
+        .then(response => response.json())
+        .then(data => console.log(data)) 
+    */
+
+    return (
+        <>
+            <h1>Postulación para Nimble Gravity</h1>
+            <PositionList />
+        </>
+    )
 }
 
 export default App
---
+++ src/components/PositionList.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import '../PositionList.css'
+
+export const PositionList = () => {
+    const URL_BASE = 'https://botfilter-h5ddh6dye8exb7ha.centralus-01.azurewebsites.net'
+
+    const [positions, setPositions] = useState([])
+
+    useEffect(() => {
+        const fetchPositions = async() => {
+                try {
+                    const response = await fetch(URL_BASE + '/api/jobs/get-list')
+                    const data = await response.json()
+                    setPositions(data)
+                } catch (error) {
+                    console.error(error)
+                }
+            }
+        
+        fetchPositions()
+    }, [])
+    
+    return (
+        <>
+            <h2>Listado de Posiciones</h2>
+            <ul className="list">
+                {positions?.map((position) => (  
+                    <ul className="item">
+                        <li key={position.id} id="title">Título: {position.title}</li>
+                        <li><input type="text" placeholder="Repositorio GitHub" /></li>
+                        <li><button>Submit</button></li>
+                    </ul>
+                ))}
+            </ul>
+        </>
+    )
+}
+
